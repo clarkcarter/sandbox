d834e284e79d467fd65b5aaa46a05b5536de8b2a
Clark Carter <clark@clarkcarter.com>
2017-08-31 07:37:24 +1000

    Changed Greeting and Presentational to stateless functional components

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-class Greeting extends React.Component {
-	render() {
-		return (
-			<div>
-				This is my greeting: {this.props.greeting}
-			</div>
-		)
-	}
+const Greeting = (props) => {
+	return (
+		<div>
+			This is my greeting: {props.greeting}
+		</div>
+	)
 }
-
 export default Greeting
--- src/Presentational.js
+++ src/Presentational.js
@@ -2,17 +2,13 @@ import React from 'react';
 import Greeting from './Greeting';
 import Input from './Input';
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h1>This is my amazing app</h1>
-        <Greeting greeting={this.props.greeting}/>
-        <Input onChange={this.props.onChange}/>
-      </div>
-    );
-  }
+const App = (props) => {
+  return (
+    <div>
+      <h1>This is my amazing app</h1>
+      <Greeting greeting={props.greeting}/>
+      <Input onChange={props.onChange}/>
+    </div>
+  );
 }
-
 export default App;
