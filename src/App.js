6c0024e9567b30da89d3f7027c6135cd36859e23
Clark Carter <clark@clarkcarter.com>
2017-09-04 11:21:01 +1000

    Making a timer (badly)
    Lots of bugs still to fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import { MiddleMan } from './MiddleMan';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: 'Replace this text'
-    };
-    this.handleChange = this.handleChange.bind(this);
+      time: 0,
+      clicked: false
+    }
   }
 
-  handleChange(e) {
-      this.setState({ text: e.target.value });
+  timer = () => {
+    const newTime = this.state.time + 1;
+    this.setState({
+      time: newTime
+    });
   }
 
-  componentDidUpdate(){
-      if (this.state.text.length === 0) {
-        this.setState({
-          text: "Replace this text"
-        });
-      }
+  handleTimer = () => {
+    const newTime = setInterval(this.timer,1000);
+
+    if (this.state.clicked) {
+      clearInterval(this.timer);
+      this.setState({
+        time: 0,
+        clicked: false
+      });
+    } else {
+      this.setState({
+        time: newTime,
+        clicked: true
+      });
+    }
   }
 
   render() {
-    return <MiddleMan onChange={this.handleChange} text={this.state.text}/>;
+    const {time, clicked} = this.state;
+    return (
+      <div>
+       <h1>You clicked the button {time} seconds ago.</h1>
+       <button onClick={this.handleTimer}>{clicked ? 'Stop' : 'Start'}</button>
+      </div>
+    )
   }
 }
 
